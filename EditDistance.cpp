f2c8ce2db1703d3b1c5d75eaf0c59a918c262eb5
Gordon Huynh <37429855+go-hyun77@users.noreply.github.com>
2019-11-18 21:41:08 -0800

    Add files via upload

---
+++ EditDistance.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void EditDistance(string x, string y);
+
+int main() {
+	string input1;
+	string input2;
+
+	cout << "Enter 1st string: ";
+	cin >> input1;
+	cout << "Enter 2nd string: ";
+	cin >> input2;
+
+	EditDistance(input1, input2);
+
+
+
+	system("pause");
+
+		
+	
+}
+
+void EditDistance(string x, string y) {
+
+
+
+}
