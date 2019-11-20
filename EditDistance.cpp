754351ef03a421382896a5a9288cda5edd8e8602
jimmyxuan <jummyxuan@gmail.com>
2019-11-20 15:39:58 -0800

    Update EditDistance.cpp

--- EditDistance.cpp
+++ EditDistance.cpp
@@ -1,31 +1,61 @@
 #include <iostream>
 #include <string>
+#include <algorithm> 
 
 using namespace std;
 
 void EditDistance(string x, string y, int a, int b);
 
-int main() {
-	string input1;
-	string input2;
+int main()
+{
+    string input1;
+    string input2;
 
-	cout << "Enter 1st string: ";
-	cin >> input1;
-	cout << "Enter 2nd string: ";
-	cin >> input2;
+    cout << "Enter 1st string: ";
+    cin >> input1;
+    cout << "Enter 2nd string: ";
+    cin >> input2;
 
-	EditDistance(input1, input2, input1.length(), input2.length());
+    cout <<"edit distance of: " << EditDistance(input1, input2, input1.length(), input2.length());
 
-
-
-	system("pause");
-
-		
-	
+    system("pause");
 }
 
-void EditDistance(string x, string y, int a, int b) {
+void EditDistance(string x, string y, int a, int b)
+{
+    string1 = x; 
+    string2 = y; 
+    int** table = new int*[a];        //create 2d array for storing words
+    for (int i = 0; i < a; i++) {
+        table[i] = new int[b];
+    }
 
-	
-	
+    int distance = 0; 
+
+    for (int k = 0; k < a+1; k++)
+    {
+        table[k]][0] = k
+    }
+
+    for (int j = 0; k < b+1; j++)
+    {
+        table[0][j] = j
+    }
+
+    for (int l = 1; l < b+1; l++)
+    {
+        for(int m = 1; m <a+1; m++)
+        {
+            if(string1[m-1] == string2[l-1])
+            {
+                table[m][l] = table[m-1][l-1]; 
+            }
+            else
+            {
+                table[m][l] = min(table[m-1][l-1], min(table[m-1][l], table[m][l-1])); 
+            }
+        }
+    }
+
+    return table[a][b];
 }
