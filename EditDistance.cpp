ce7b138a9f1a9ebcb29d8ad4399fd9ee58a37dd5
Gordon Huynh <37429855+go-hyun77@users.noreply.github.com>
2019-11-21 15:25:53 -0800

    Update EditDistance.cpp

--- EditDistance.cpp
+++ EditDistance.cpp
@@ -4,60 +4,78 @@
 
 using namespace std;
 
-int EditDistance(string x, string y, int a, int b);
+int EditDistance(string x, string y);
 
 int main()
 {
-    string input1;
-    string input2;
+	string input1;
+	string input2;
 
-    cout << "Enter 1st string: ";
-    cin >> input1;
-    cout << "Enter 2nd string: ";
-    cin >> input2;
+	cout << "Enter 1st string: ";
+	cin >> input1;
+	cout << "Enter 2nd string: ";
+	cin >> input2;
 
-    cout <<"edit distance of: " << EditDistance(input1, input2, input1.length(), input2.length());
+	cout << endl << "Edit distance of: " << EditDistance(input1, input2) << endl;
 
-    system("pause");
+	system("pause");
 }
 
-void EditDistance(string x, string y, int a, int b)
-{
-    string1 = x; 
-    string2 = y; 
-    int** table = new int*[a];        //create 2d array for storing words
-    for (int i = 0; i < a; i++) {
-        table[i] = new int[b];
+int EditDistance(string x, string y) {
+	int m = x.size();
+	int n = y.size();
 
-    }
+	int** table = new int*[m + 1];					//create dynamic 2d array for edit distance table
+	for (int i = 0; i < (m + 1); i++) {
+		table[i] = new int[n + 1];
+	}
 
-    int distance = 0; 
+	
+	for (int i = 0; i < (m + 1); i++) {				//fill 1st row with 0 1 2 3 4...
+		table[i][0] = i;
+	}
+	
+	for (int j = 0; j < (n + 1); j++) {				//fill 1st column with 0 1 2 3 4...
+		table[0][j] = j;
+	}
 
-    for (int k = 0; k < a+1; k++)
-    {
-        table[k]][0] = k;
-    }
+	for (int i = 1; i < (m + 1); i++) {				//recursive edit distance 
+		for (int j = 1; j < (n + 1); j++) {
+			if (x[i - 1] == y[j - 1]) {
+				table[i][j] = table[i - 1][j - 1];		
+			}
+			else {
+				table[i][j] = 1 + min(table[i - 1][j - 1], min(table[i - 1][j], table[i][j - 1]));
+			}
+		}
+	}
 
-    for (int j = 0; k < b+1; j++)
-    {
-        table[0][j] = j;
+	int distance = table[m][n];
 
-    }
+	cout << "    ";
 
-    for (int l = 1; l < b+1; l++)
-    {
-        for(int m = 1; m <a+1; m++)
-        {
-            if(string1[m-1] == string2[l-1])
-            {
-                table[m][l] = table[m-1][l-1]; 
-            }
-            else
-            {
-                table[m][l] = min(table[m-1][l-1], min(table[m-1][l], table[m][l-1])); 
-            }
-        }
-    }
+	for (int i = 0; i < (m + 1); i++) {
+		cout << table[i][0] << "   ";
+	}
+	
+	cout << endl; 
 
-    return table[a][b];
+	for (int i = 0; i < m + 1; i++) {				// table print
+		cout << endl;
+
+		cout << table[i][0] << "   ";
+
+		for (int j = 0; j < n + 1; j++) {
+			cout << table[i][j] << "   ";
+			}
+
+		cout << endl;
+	}
+
+	for (int i = 0; i < m + 1; i++) {				//free memory	
+		delete[] table[i];
+	}
+	delete[] table;
+
+	return distance;
 }
