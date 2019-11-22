a297ad1bd31852b6c261643d9b43f928132e4236
jimmyxuan <jummyxuan@gmail.com>
2019-11-21 16:10:36 -0800

    Update EditDistance.cpp

--- EditDistance.cpp
+++ EditDistance.cpp
@@ -1,81 +1,151 @@
 #include <iostream>
 #include <string>
-#include <algorithm> 
-
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-int EditDistance(string x, string y);
+void EditDistance(string x, string y, int a, int b);
+
+enum step_direction
+{
+    STEP_DIRECTION_BOTTTOMRIGHT,
+    STEP_DIRECTION_RIGHT,
+    STEP_DIRECTION_DOWN
+};
+
+vector<step_direction> Alignment(int *arr[], int a, int b);
 
 int main()
 {
-	string input1;
-	string input2;
+    string input1;
+    string input2;
 
-	cout << "Enter 1st string: ";
-	cin >> input1;
-	cout << "Enter 2nd string: ";
-	cin >> input2;
+    cout << "Enter 1st string: ";
+    cin >> input1;
+    cout << "Enter 2nd string: ";
+    cin >> input2;
 
-	cout << endl << "Edit distance of: " << EditDistance(input1, input2) << endl;
+    EditDistance(input1, input2, input1.length(), input2.length());
 
-	system("pause");
+    return 0;
 }
 
-int EditDistance(string x, string y) {
-	int m = x.size();
-	int n = y.size();
+void EditDistance(string x, string y, int a, int b)
+{
+    string string1 = x;
+    string string2 = y;
 
-	int** table = new int*[m + 1];					//create dynamic 2d array for edit distance table
-	for (int i = 0; i < (m + 1); i++) {
-		table[i] = new int[n + 1];
-	}
+    int **table = new int *[a + 1]; //create 2d array for storing words
+    for (int i = 0; i < a + 1; i++)
+    {
+        table[i] = new int[b + 1];
+    }
 
-	
-	for (int i = 0; i < (m + 1); i++) {				//fill 1st row with 0 1 2 3 4...
-		table[i][0] = i;
-	}
-	
-	for (int j = 0; j < (n + 1); j++) {				//fill 1st column with 0 1 2 3 4...
-		table[0][j] = j;
-	}
+    int distance = 0;
 
-	for (int i = 1; i < (m + 1); i++) {				//recursive edit distance 
-		for (int j = 1; j < (n + 1); j++) {
-			if (x[i - 1] == y[j - 1]) {
-				table[i][j] = table[i - 1][j - 1];		
-			}
-			else {
-				table[i][j] = 1 + min(table[i - 1][j - 1], min(table[i - 1][j], table[i][j - 1]));
-			}
-		}
-	}
+    for (int k = 0; k < a + 1; k++)
+    {
+        table[k][0] = k;
+    }
+    cout << endl;
+    for (int j = 0; j < b + 1; j++)
+    {
+        table[0][j] = j;
+    }
 
-	int distance = table[m][n];
+    for (int l = 1; l < b + 1; l++)
+    {
+        for (int m = 1; m < a + 1; m++)
+        {
+            if (string1[m - 1] == string2[l - 1])
+            {
+                table[m][l] = table[m - 1][l - 1];
+            }
+            else
+            {
+                table[m][l] = 1 + min(table[m - 1][l - 1], min(table[m - 1][l], table[m][l - 1]));
+            }
+        }
+    }
 
-	cout << "    ";
+    for (int i = 0; i < a + 1; i++)
+    { // table print
+        cout << endl;
 
-	for (int i = 0; i < (m + 1); i++) {
-		cout << table[i][0] << "   ";
-	}
-	
-	cout << endl; 
+        cout << table[i][0] << "   ";
 
-	for (int i = 0; i < m + 1; i++) {				// table print
-		cout << endl;
+        for (int j = 0; j < b + 1; j++)
+        {
+            cout << table[i][j] << "   ";
+        }
 
-		cout << table[i][0] << "   ";
+        cout << endl;
+    }
 
-		for (int j = 0; j < n + 1; j++) {
-			cout << table[i][j] << "   ";
-			}
+    cout << "edit distance of: " << table[a][b] << endl;
+    vector<step_direction> fixed_align = Alignment(table, a, b);
+    if (!fixed_align.empty())
+    {
+        cout << fixed_align[0] << endl;
+    }
+    else
+        cout << "empty!" << endl;
+}
 
-		cout << endl;
-	}
+vector<step_direction> Alignment(int *arr[], int a, int b)
+{
+    vector<step_direction> temp;
+    int row = a;
+    int column = b;
+    //cout <<a <<"   "  <<b <<endl;
+    cout << "column:" << column << endl
+         << "row: " << row << endl;
+    if (column == 0 && row == 0)
+    {
+        return temp;
+    }
+    else if (column == 0)
+    {
+        temp.push_back(STEP_DIRECTION_DOWN);
+        row--;
+        return Alignment(arr, row, column);
+    }
+    else if (row == 0)
+    {
+        temp.push_back(STEP_DIRECTION_RIGHT);
+        column--;
+        return Alignment(arr, row, column);
+    }
+    else
+    {
+        if ((arr[row - 1][column - 1] <= arr[row][column]) && ((arr[row - 1][column] >= arr[row][column]) && (arr[row][column - 1] >= arr[row][column])))
+        {
+            temp.push_back(STEP_DIRECTION_BOTTTOMRIGHT);
+            row--;
+            column--;
+            cout << "diag?" << endl;
+            return Alignment(arr, row, column);
+        }
+        else if ((arr[row - 1][column] < arr[row][column]) && (arr[row - 1][column] <= arr[row][column - 1]))
+        {
+            temp.push_back(STEP_DIRECTION_DOWN);
+            row--;
+            cout << "right?" << endl;
+            return Alignment(arr, row, column);
+        }
+        else if ((arr[row][column - 1] < arr[row][column]) && (arr[row][column - 1] <= arr[row - 1][column]))
+        {
+            temp.push_back(STEP_DIRECTION_RIGHT);
+            column--;
+            cout << "down" << endl;
+            return Alignment(arr, row, column);
+        }
+        else
+        {
+            cout << "Unexpected Check" << endl;
+            return temp;
+        }
+    }
 
-	for (int i = 0; i < m + 1; i++) {				//free memory	
-		delete[] table[i];
-	}
-	delete[] table;
-
-	return distance;
+    return temp;
 }
